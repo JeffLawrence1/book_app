e205afd28a88cd90d41ea5aa700be499a7434791
JeffLawrence1 <jefflaw13@hotmail.com>
2019-05-10 15:42:26 -0700

    hide button fixed again, delete button added

--- public/js/app.js
+++ public/js/app.js
@@ -4,11 +4,9 @@ $('.viewBut').on('click', function(event) {
 
   let className = $(event.target).siblings('form').attr('class');
 
-  if(event.target.className.includes('viewBut')){
-    if (className.includes('hiddenForm')) {
-      $(event.target).siblings('form').removeClass('hiddenForm');
-    } else {
-      $(event.target).siblings('form').addClass('hiddenForm');
-    }
+  if (className.includes('hiddenForm')) {
+    $(event.target).siblings('form').removeClass('hiddenForm');
+  } else {
+    $(event.target).siblings('form').addClass('hiddenForm');
   }
 });
--- server.js
+++ server.js
@@ -103,6 +103,7 @@ function getBook(request, response){
 
   return client.query(SQL, values)
     .then(result => {
+
       response.render('pages/books/show', {results: result.rows[0]});
     })
     .catch (err => errorPage(err, response));
